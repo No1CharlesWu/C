20122b6b64b54b6470035a25242753727614c3d2
no1charleswu <no1charleswu@163.com>
2018-09-07 13:46:56 +0800

    13

---
+++ leetcode/13_RomanToInteger.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution
+{
+  public:
+    int romanToInt(string s)
+    {
+        int res = 0;
+
+        unordered_map<char, int> map = {{'I', 1},
+                                        {'V', 5},
+                                        {'X', 10},
+                                        {'L', 50},
+                                        {'C', 100},
+                                        {'D', 500},
+                                        {'M', 1000}};
+
+        res = map[s[0]]
+        for(int i = 1;i < s.length();i++)
+
+        return res;
+    }
+};
+
+int main()
+{
+
+    // system("pause");
+    return 0;
+}
