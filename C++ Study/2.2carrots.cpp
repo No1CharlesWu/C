09e43ead319d739c0103f6c45556cd2677f1c311
No1ChalresWu <No1CharlesWu@163.com>
2019-04-09 14:39:14 +0800

    C++ Primer

---
+++ C++ Study/2.2carrots.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int carrots = 25;
+
+    cout << "I have " << carrots << " carrots." << endl;
+    carrots--;
+    cout << "Crunch,Crunch, I have " << carrots << " carrots." << endl;
+
+    return 0;
+}
